a6b89bf8e2085f2b633ab5536ce3b8f5841c6d83
kirisame2018 <35220134+kurumifan@users.noreply.github.com>
2018-05-03 09:38:33 +0800

    稍作修改

--- lib/pages/acg_page.dart
+++ lib/pages/acg_page.dart
@@ -20,7 +20,8 @@ class _AcgPageState extends State<AcgPage> {
   /// 打开下载页面
   void hanldeTap(String uri) {
     Navigator.push(context, new MaterialPageRoute(builder: (BuildContext context) {
-      return new Container(
+      return new Center(
+       
         child: new Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -29,10 +30,14 @@ class _AcgPageState extends State<AcgPage> {
                     placeholder: new CircularProgressIndicator(),
                     errorWidget: new Icon(Icons.error),
                   ),
-            new RaisedButton(
-                onPressed: ()=>handleDownload(uri),
-                child: new Text('下载')
-            )
+            // new RaisedButton(
+            //     onPressed: ()=>handleDownload(uri),
+            //     child: new Text('下载')
+            // )
+            // new FlatButton(
+            //   onPressed: ()=>handleDownload(uri),
+            //    child: new Icon(Icons.access_alarm),
+            // )
           ],
         ),
       );
@@ -64,22 +69,22 @@ class _AcgPageState extends State<AcgPage> {
   }
 
   Widget createListImage(BuildContext context, AsyncSnapshot snapshot) {
-    List<String> eee = snapshot.data;
+    List<String> imgs = snapshot.data;
     return new GridView.extent(
       maxCrossAxisExtent: 160.0,
       mainAxisSpacing: 4.0,
       crossAxisSpacing: 4.0,
       padding: const EdgeInsets.all(4.0),
       children: new List<Widget>.generate(
-        eee.length,
+        imgs.length,
         (a) => new GestureDetector(
           onTap: (){
             hanldeTap("https://ab16-1256318515.cos.ap-chengdu.myqcloud.com/" +
-                    eee[a]);
+                    imgs[a]);
           },
           child: new CachedNetworkImage(
                 imageUrl: "https://ab16-1256318515.cos.ap-chengdu.myqcloud.com/" +
-                    eee[a],
+                    imgs[a],
                 placeholder: new CircularProgressIndicator(),
                 errorWidget: new Icon(Icons.error),
               ),
